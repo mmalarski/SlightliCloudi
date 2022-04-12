dab4a4627fe7fd906a411b0ff7c1d69eb08467a9
Mateusz Roszkowski <blackspider1@o2.pl>
2022-04-12 20:06:03 +0200

    Spring init

---
+++ src/main/java/kw/hk/mm/mr/slightlicloudi/SlightliCloudiApplication.java
@@ -0,0 +1,13 @@
+package kw.hk.mm.mr.slightlicloudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlightliCloudiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlightliCloudiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kw/hk/mm/mr/slightlicloudi/SlightliCloudiApplicationTests.java
@@ -0,0 +1,13 @@
+package kw.hk.mm.mr.slightlicloudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlightliCloudiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
