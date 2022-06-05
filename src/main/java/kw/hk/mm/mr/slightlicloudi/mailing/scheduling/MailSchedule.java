aefd22db05b51c44c317d1bb38b1be3de0e504d5
Hanna-J-K <hanna.kraska11@gmail.com>
2022-06-05 14:41:37 +0200

    added sendMail method

--- src/main/java/kw/hk/mm/mr/slightlicloudi/mailing/scheduling/MailSchedule.java
+++ src/main/java/kw/hk/mm/mr/slightlicloudi/mailing/scheduling/MailSchedule.java
@@ -1,9 +1,32 @@
 package kw.hk.mm.mr.slightlicloudi.mailing.scheduling;
 
+import kw.hk.mm.mr.slightlicloudi.mailing.MailService;
+import kw.hk.mm.mr.slightlicloudi.mailing.MailType;
+import kw.hk.mm.mr.slightlicloudi.user.UserPreferences;
+
+import javax.mail.MessagingException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MailSchedule {
 
+    MailService mailService;
 
+    public void sendMail(UserPreferences userPreferences, MailType mailType) throws MessagingException {
+        String templatePath = "";
+        Map<String, Object> templateModel = new HashMap<>();
+        if(mailType == MailType.DAILY) {
+            templatePath = "daily-mail-template.html";
+        }
+        if(mailType == MailType.WEEKLY) {
+            templatePath = "weekly-mail-template.html";
+        }
+        if(mailType == MailType.WEEKENDS) {
+            templatePath = "weekendly-mail-template.html";
+        }
 
+        mailService.sendMessageUsingThymeleafTemplate(userPreferences.getUser().toString(), "Weather Forecast", templatePath, templateModel);
+    }
 
 
 }
